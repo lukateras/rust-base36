183f6bd8a6f53f5f3698daf0fd63424986983688
Luka Teras <luka@sdf.org>
2024-12-08 21:53:39 +0000

    Document the crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,21 @@
+//! Base36 encoder/decoder.
+
 use num_bigint::{BigUint, ParseBigIntError};
 use num_traits::Num;
 
 const RADIX: u32 = 36;
 
-pub fn decode(s: &str) -> Result<Vec<u8>, ParseBigIntError> {
+/// Error during `decode`.
+pub type DecodeError = ParseBigIntError;
+
+/// Decodes a Base36 string into a byte array.
+///
+/// Returns `DecodeError` if the string is not Base36.
+pub fn decode(s: &str) -> Result<Vec<u8>, DecodeError> {
     Ok(BigUint::from_str_radix(s, RADIX)?.to_bytes_be())
 }
 
+/// Encodes a byte array into a Base36 string (lowercase).
 pub fn encode(buf: &[u8]) -> String {
     BigUint::from_bytes_be(buf).to_str_radix(RADIX)
 }
